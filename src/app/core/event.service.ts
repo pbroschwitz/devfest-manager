eaf4e46c908aa2063c481ad08275aac3109ec8b8
Peter Broschwitz <p.broschwitz@reply.de>
2026-09-16 13:16:14 +0200

    feat: add API_URL as InjectionToken

--- src/app/core/event.service.ts
+++ src/app/core/event.service.ts
@@ -1,12 +1,14 @@
 import { HttpClient, httpResource } from '@angular/common/http';
 import { inject, Injectable, Signal } from '@angular/core';
 import { DevFestEvent } from '../models/event.model';
+import { API_URL } from './tokens';
 
 @Injectable({
   providedIn: 'root',
 })
 export class EventsService {
-  private apiUrl = 'http://localhost:3000/events';
+  private url = inject(API_URL);
+  private apiUrl = `${this.url}/events`;
   private readonly http = inject(HttpClient);
 
   getEventsResource(query: Signal<string>) {
